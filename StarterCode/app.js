c7db43f98ef2bfc6664a77eea00231032d50b4c7
Deborah <deborahigbalajobi@yahoo.com>
2020-05-26 18:51:23 -0400

    code so far

--- StarterCode/app.js
+++ StarterCode/app.js
@@ -15,7 +15,7 @@ var width = svgWidth - margin.left - margin.right;
 var height = svgHeight - margin.top - margin.bottom;
 
 // Create an SVG wrapper, append an SVG group that will hold our chart, and shift the latter by left and top margins.
-var svg = d3.select(".chart")
+var svg = d3.select("#scatter")
   .append("svg")
   .attr("width", svgWidth)
   .attr("height", svgHeight);
@@ -31,38 +31,70 @@ d3.csv("data.csv").then(function(HData) {
     HData.forEach(function(data) {
         data.poverty = +data.poverty;
         data.healthcare = +data.healthcare;
-    })
+        data.age = +data.age;
+        data.smokes = +data.smokes;
+        data.abbr = data.abbr;
 
     });
 
-// // Create circles 
-// var circlesGroup = chartGroup.selectAll("circle")
-// .data(Data)
-// .enter()
-// .append("circle")
-// .attr("cx", d => xLinearScale(d.poverty))
-// .attr("cy", d => yLinearScale(d.healthcare))
-// .attr("r", "15")
-// .attr("fill", "pink")
-// .attr("opacity", ".5");
+    
+    // Step 2: Create scale functions
+    // ==============================
+    var xLinearScale = d3.scaleLinear()
+      .domain([20, d3.max(HData, d => d.poverty)])
+      .range([0, width]);
 
-  
-// // Create an SVG wrapper, append an SVG group that will hold our chart, and shift the latter by left and top margins.
+    var yLinearScale = d3.scaleLinear()
+      .domain([0, d3.max(HData, d => d.healthcare)])
+      .range([height, 0]);
+
+      // Step 3: Create axis functions
+    // ==============================
+    var bottomAxis = d3.axisBottom(xLinearScale);
+    var leftAxis = d3.axisLeft(yLinearScale);
+
+    // Step 4: Append Axes to the chart
+    // ==============================
+    chartGroup.append("g")
+      .attr("transform", `translate(0, ${height})`)
+      .call(bottomAxis);
+
+    chartGroup.append("g")
+      .call(leftAxis);
+
+      // Step 5: Create Circles
+    // ==============================
+    var circlesGroup = chartGroup.selectAll("circle")
+    .data(HData)
+    .enter()
+    .append("circle")
+    .attr("cx", d => xLinearScale(d.poverty))
+    .attr("cy", d => yLinearScale(d.healthcare))
+    .attr("r", "11")
+    .attr("fill", "pink")
+    .attr("opacity", ".5");
+
+    var textGroup = chartGroup.selectAll("text")
+    .data(HData)
+    .enter()
+    .append("text")
+    .style("fill", "black")
+    .attr('x',d => xLinearScale(d.poverty))
+    .attr('y',d => yLinearScale(d.healthcare))
+    .attr("dy", ".35em") 
+    .attr("text-anchor", "middle")
+    .text(d => d.abbr);
+    console.log(HData)
+
+    // Step 6: Initialize tool tip
+    // ==============================
+    var toolTip = d3.tip()
+      .attr("class", "tooltip")
+      .offset([80, -60])
+      .html(function(d) {
+        return (`${d.rockband}<br>Hair length: ${d.hair_length}<br>Hits: ${d.num_hits}`);
+      });
 
 
-// // set borders in svg
+    });
 
-// function init(){
-//     djdata = "";
-//     d3.csv("data.csv").then(healthData=> {
-//         console.log(healthData)
-//         djdata= healthData
-//     });
-//     djdata.forEach(function(state){
-//         state.poverty = +state.poverty;
-//         state.abbr = state.abbr;
-//         state.income = +state.income;
-//         state.obesity= +state.obesity;
-//     })
-//     };
-//     init();
